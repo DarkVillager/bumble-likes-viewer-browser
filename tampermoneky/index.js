326841e9028a25339628ca5a260ee95d7f2a3120
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2025-04-05 14:31:55 +0200

    feat: beginning

---
+++ tampermoneky/index.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        Tinder Unblur
+// @namespace   https://tampermonkey.net/
+// @match       https://*.bumble.com/*
+// @version     1.0.0
+// @author      Dark_Ville
+// @description show votes, show online status, and change location on bumble
+// ==/UserScript==
