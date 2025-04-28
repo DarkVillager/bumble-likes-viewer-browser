19ee37841b415b2090a1b42d79358bfbf700a9e1
Dark_Ville <42322979+DarkVillager@users.noreply.github.com>
2025-04-28 13:34:23 +0200

    Update index.js

--- tampermoneky/index.js
+++ tampermoneky/index.js
@@ -6,3 +6,14 @@
 // @author      Dark_Ville
 // @description show votes, show online status, and change location on bumble
 // ==/UserScript==
+
+const {hostname} = document.location;
+
+
+
+(async function() {
+    'use strict';
+const url = `https://${hostname}/mwebapi.phtml?SERVER_GET_ENCOUNTER`
+const fetched = await fetch(url)
+console.log(fetched)
+})();
